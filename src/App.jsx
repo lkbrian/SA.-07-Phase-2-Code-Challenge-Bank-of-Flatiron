420566dbe374dc54c93c5cdc50eaf910de77286d
lkbrian <lkbrian742@gmail.com>
2024-02-03 10:20:49 +0300

    starter files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import SearchBar from "./components/SearchBar" 
+import Table from './components/Table'
+import Form from './components/Form'
+
+function App() {
+
+  return (
+    <>
+    <SearchBar/>
+    <Form/>
+    <Table/>
+    
+     
+    </>
+  )
+}
+
+export default App
